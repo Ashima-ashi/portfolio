2b6d7633c7b45f6ba30ffcfb1742bc5bf4b93902
ashimagarg118@gmail.com <ashima1397.be22@citkara.edu.in>
2024-02-04 19:38:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings : ['Frontend Developer' , 'C++ Programer', 'Core-Executive'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay : 1000,
+    loop : true
+})
